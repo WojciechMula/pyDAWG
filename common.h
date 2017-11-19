7c41128af627a92c5141dc5bf5eb8ad9140f87b5
Paulo Villegas <paulo.vllgs@gmail.com>
2017-11-19 22:56:04 +0100

    fixed memory problems  * use HASH_FREE macro (defined as free in dawg.h) for memory that was allocated with HASH_ALLOC (using malloc) inside hash/hashtable.c  * fixed DAWG_clear(): can't use DAWG_traverse_DFS_once() for memory free, since the same node could be reached more than once (and when checking if it had been visited, we could then be accessing a node that has already been freed, hence behaviour would be unpredictable) minor changes:  * commented out puts() line  * DEBUG definition done in setup.py file

--- common.h
+++ common.h
@@ -47,7 +47,6 @@
 #	define	UNUSED
 #endif
 
-#define DEBUG
 #ifdef DEBUG
 #	include <assert.h>
 #	define	ASSERT(expr)	do {if (!(expr)) {printf("%s:%s:%d - '%s' failed!\n", __FILE__, __FUNCTION__, __LINE__, #expr); abort();} }while(0)
@@ -89,9 +88,17 @@ void memfree(void* addr) {
     printf("free %p\n", addr);
     PyMem_Free(addr);
 }
+
+void *memcalloc(size_t nmemb, size_t size) {
+    void* addr = PyMem_Calloc(nmemb, size);
+    printf("calloc %p %u\n", addr, nmemb*size);
+    return addr;
+}
+
 #else
 #   define memalloc PyMem_Malloc
 #   define memfree  PyMem_Free
+#   define memcalloc PyMem_Calloc
 #endif
 
 #if defined(_WIN32) || defined(_WIN64)
